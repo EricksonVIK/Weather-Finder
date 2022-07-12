a9ae49a273d00b139a93e95d4f817e79e14c45da
Erikson <chris.s.erickson@gmail.com>
2022-07-12 09:35:33 -0400

    pseudo code

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -1,12 +1,30 @@
 // create variables for dom's
 var userFormEl = document.querySelector("#search-form");
 
+// array to store city search history
+var cityHistory=[]
+
 // form submit function
 var citySubmitAction = function(event){
-    
+    // prevent page refresh
     event.preventDefault();
-
+    // get value from input
+    var citySelect =userFormEl.value.trim();
 }
+
+// store input and display  - make "clickable"
+
+// event function to redisplay city on click
+
+// fetch function
+
+// display result function w loop
+
+// account for errors
+
+// add event listener for saved directory
+// add event listner for city input
+userFormEl.addEventListner("submit", citySubmitAction())
 
 
 
