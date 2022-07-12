562c59f5ac6797ce41c5b62895f132e4cd7536e2
Erikson <chris.s.erickson@gmail.com>
2022-07-12 09:17:36 -0400

    added html, general style sheet, started js

---
+++ assets/js/mainpage.js
@@ -0,0 +1,12 @@
+// create variables for dom's
+var userFormEl = document.querySelector("#search-form");
+
+// form submit function
+var citySubmitAction = function(event){
+    
+    event.preventDefault();
+
+}
+
+
+
