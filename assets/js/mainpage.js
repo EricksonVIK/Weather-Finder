ca1172a87ebf9f4072525cdde9132230d5e81959
Erikson <chris.s.erickson@gmail.com>
2022-07-12 13:04:13 -0400

    both geocode and lat&lon fetches work

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -14,28 +14,48 @@ var submitCS = function(event){
     // prevent page from refreshing
     event.preventDefault();
     // get value from input
-    console.log(event)
-}
+    // console.log(event) - worked
+    var city = cityInputEl.value.trim();
+    var state = stateInputEl.value.trim();
 
-// userFormEl.addEventListener("submit", submitCS);
+    if (city, state){
+        getLatLonInfo(city, state)
+        cityInputEl.value = "";
+        stateInputEl.value = "";
+    }else{
+        alert("Please enter valid city and state.");
+    };
+};
+
 
 // store input and display  - make "clickable"
 
 // event function to redisplay city on click
 
-// fetch function
-var getLatLonInfo = function (city){
+// fetch functions
+var getWeatherInfo = function (){
+    console.log("this has been called");
+    var apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=0714ff4099a70754b88ad38fab16cccd"
+    fetch (apiUrl)
+    .then(function (response){
+        if (response.ok){
+            // console.log(response);worked
+            response.json().then(function(data){
+                console.log(data);
+            })
+        };
+    })
+}
+var getLatLonInfo = function (city, state){
     console.log("called");
     // format weather api
     // geo code to get long and lat
-    var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + ",NC,USA&limit=1&appid=0714ff4099a70754b88ad38fab16cccd"
+    var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + ",USA&limit=1&appid=0714ff4099a70754b88ad38fab16cccd"
     fetch(geoUrl)
         .then(function(response){
             if (response.ok){
-                // console worked
-                console.log(response);
+                // console.log(response); worked
                 response.json().then(function(data){
-                    // worked
                     console.log(data);
                 });
             } else {
@@ -45,6 +65,7 @@ var getLatLonInfo = function (city){
 }
 // hard coding city worked
 // getLatLonInfo("Durham");
+getWeatherInfo();
 // display result function w loop
 
 // account for errors
